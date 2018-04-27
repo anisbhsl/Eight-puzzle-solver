8d8018554daf0a1c01e9befcdd43d2d5240a253d
bhusal <bhusal.anish12@gmail.com>
2018-04-27 21:50:54 +0545

    solver implementation

--- solver.py
+++ solver.py
@@ -2,24 +2,27 @@
 
 from astaralgorithm import *   
 
-print ("8 PUZZLE SOLVER")
+print ("--------------8 PUZZLE SOLVER-------------------\n")
 
 #################### Input ###########################################
 
 #puz=puzzle(startNode)
-startNode=['0','1','2','3','4','5','6','7','8']
+startNode=['1','0','2','3','4','5','6','7','8']
 puz=puzzle(startNode)
-
-puz.shuffle(puz.startNode)
-nextNode=puz.getNextNode()
-
-
-while (nextNode!=puz.goalNode):
-	puz.shuffle(nextNode)
+if(startNode!=puz.goalNode):
+	puz.shuffle(puz.startNode)
 	nextNode=puz.getNextNode()
+	
+
+	while (nextNode!=puz.goalNode):
+		puz.shuffle(nextNode)
+		nextNode=puz.getNextNode()
 
 
-for i in nextNode:
-	print(i,end=',')
+	for i in nextNode:
+		print(i,end=',')
+
+else:
+	print ("The start state is same as goal state")	
 
 
